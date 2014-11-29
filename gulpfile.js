8dde5b9181853d4dd97fcc3153eb1218cef01ebc
Fran <gotusso@gmail.com>
2014-11-29 18:05:11 +0100

    JSHint integration

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var gulp = require('gulp');
+var jshint = require('gulp-jshint');
 var traceur = require('gulp-traceur');
 var clean = require('gulp-clean');
 
@@ -8,13 +9,20 @@ gulp.task('default', ['clean'], function() {
 	gulp.start('build');
 });
 
-gulp.task('build', function() {
-  return gulp.src('src/**/*.js')
-    .pipe(traceur())
-    .pipe(gulp.dest('out'));
-});
-
 gulp.task('clean', function () {
   return gulp.src('out', { read: false })
     .pipe(clean());
+});
+
+gulp.task('lint', function() {
+  return gulp.src('src/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(jshint.reporter('fail'));
+});
+
+gulp.task('build', ['lint'], function() {
+  return gulp.src('src/**/*.js')
+    .pipe(traceur())
+    .pipe(gulp.dest('out'));
 });
