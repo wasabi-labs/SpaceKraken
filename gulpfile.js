1ae2d68943e6f50a244fccee282442dc1ff2bcbb
Franco Gotusso <gotusso@gmail.com>
2014-12-21 13:41:39 +0100

    Browserify integration with source maps

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,16 @@ var gulp = require('gulp');
 var fs = require('fs');
 var clean = require('gulp-clean');
 var jshint = require('gulp-jshint');
+var browserify = require('browserify');
+var es6ify = require('es6ify');
+var source = require('vinyl-source-stream');
 var traceur = require('gulp-traceur');
 var header = require('gulp-header');
+var filter = require('gulp-filter');
 var merge = require('merge-stream');
 var mocha = require('gulp-mocha');
+
+var pack = require('./package.json');
 
 gulp.task('default', ['clean'], function() {
     gulp.start('build');
@@ -26,7 +32,18 @@ gulp.task('lint', function() {
         .pipe(jshint.reporter('fail'));
 });
 
-gulp.task('build', ['lint', 'clean'], function() {
+gulp.task('build', ['lint'], function() {
+    return browserify({
+            entries: [es6ify.runtime, './src/index.js'],
+            debug: true
+        })
+        .transform(es6ify)
+        .bundle()
+        .pipe(source(pack.name + '.js'))
+        .pipe(gulp.dest('./out/build'));
+});
+
+gulp.task('test', function() {
     var src = gulp.src('src/**/*.js')
         .pipe(traceur())
         .pipe(gulp.dest('out/src'));
@@ -34,11 +51,10 @@ gulp.task('build', ['lint', 'clean'], function() {
         .pipe(traceur())
         .pipe(header(fs.readFileSync('test/Runner.js', 'utf8')))
         .pipe(gulp.dest('out/test'));
-    return merge(src, test);
-});
-
-gulp.task('test', ['build'], function () {
-    return gulp.src('out/test/**/*.js')
+    return merge(src, test)
+        .pipe(filter(function(file) {
+            return /test/.test(file.path);
+        }))
         .pipe(mocha({
             reporter: 'nyan'
         }));
