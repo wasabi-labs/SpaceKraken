c8c185bd34f657b4cf6aaf0731e8c86ed420d227
Franco Gotusso <gotusso@gmail.com>
2015-01-11 11:07:31 +0100

    Placeholder materials

--- app/src/render/MatchRenderer.js
+++ app/src/render/MatchRenderer.js
@@ -12,7 +12,22 @@ function factory(canvas, engine, match) {
 
     // Light
     let light = new Babylon.HemisphericLight('light1', new Babylon.Vector3(0, 1, 0), scene);
-    light.intensity = 0.5;
+    light.intensity = 0.75;
+
+    // Materials
+    let materials = {
+        planet: new Babylon.StandardMaterial("planetMaterial", scene),
+        path: new Babylon.StandardMaterial("pathMaterial", scene),
+        ground: new Babylon.StandardMaterial("groundMaterial", scene)
+    }
+
+    let placeholder = 'textures/placeholder.png';
+    materials.planet.diffuseColor = new Babylon.Color3(0.2, 0.6, 1.0);
+    materials.planet.ambientTexture = new Babylon.Texture(placeholder, scene);
+    materials.path.diffuseColor = new Babylon.Color3(0.75, 1.0, 0.75);
+    materials.path.ambientTexture = new Babylon.Texture(placeholder, scene);
+    materials.path.alpha = 0.5;
+    materials.ground.wireframe = true;
 
     // Map
     let map = match.map;
@@ -21,6 +36,7 @@ function factory(canvas, engine, match) {
 
         // Planets
         let sphere = Babylon.Mesh.CreateSphere(planet.name, 8, planet.size, scene);
+        sphere.material = materials.planet;
         sphere.position.x = - (map.width / 2) + position.x;
         sphere.position.z = - (map.height / 2) + position.y;
 
@@ -35,6 +51,7 @@ function factory(canvas, engine, match) {
             let hy = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
 
             let path = Babylon.Mesh.CreatePlane(planet.name + connection.name + 'Path', 1, scene);
+            path.material = materials.path;
             path.parent = sphere;
 
             path.position.x = - dx / 2;
@@ -48,8 +65,7 @@ function factory(canvas, engine, match) {
 
     // Debug ground
     let ground = Babylon.Mesh.CreateGround('ground', map.width, map.height, Math.max(map.width, map.height), scene);
-    ground.material = new Babylon.StandardMaterial("texture1", scene);
-    ground.material.wireframe = true;
+    ground.material = materials.ground;
 
     return scene;
 }
