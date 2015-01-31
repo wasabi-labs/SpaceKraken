8b610a47c0e4bcad58072e351ce4d3aa8785f56e
Franco Gotusso <gotusso@gmail.com>
2015-01-31 16:22:36 +0100

    Screen layout

--- app/src/index.js
+++ app/src/index.js
@@ -10,9 +10,9 @@ angular.module('Kraken', [
 .controller('MatchController', MatchController)
 .config(['$routeProvider', function($routeProvider) {
     $routeProvider
-        .when("/match", {
-        	templateUrl: "views/MatchView.html", 
-        	controller: "MatchController"
+        .when('/match', {
+        	templateUrl: 'views/MatchView.html',
+        	controller: 'MatchController'
         })
         .otherwise({
         	redirectTo: '/match'
--- app/src/ui/SceneDirective.js
+++ app/src/ui/SceneDirective.js
@@ -4,14 +4,19 @@ export default [function() {
     return {
         restrict: 'E',
         replace: true,
+        transclude: true,
         scope: {
             match: '='
         },
         template: [
-            '<canvas id="renderer"></canvas>'
+            '<div class="scene">',
+            '    <canvas class="renderer"></canvas>',
+            '    <ng-transclude></ng-transclude>',
+            '</div>'
         ].join('\n'),
         link: function($scope, $element, $attributes) {
-            $scope.renderer = new Renderer($element[0], $scope.match);
+            let canvas = $element.find('canvas')[0];
+            $scope.renderer = new Renderer(canvas, $scope.match);
         },
         controller: ['$scope', function($scope) {
 
